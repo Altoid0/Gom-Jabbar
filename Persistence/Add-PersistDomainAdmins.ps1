ad127907cbca5c64acb8ed363c060048142ba1f6
Altoid0 <Altoid0@IRON-FORGE>
2022-04-15 19:54:56 -0700

    GenericAll

--- Persistence/Add-PersistDomainAdmins.ps1
+++ Persistence/Add-PersistDomainAdmins.ps1
@@ -1,12 +1,13 @@
 function Add-PersistDomainAdmins {
     <#
     .SYNOPSIS
-    This uses the Powershell Active Directory module's New-ADUser and Add-ADGroupMember to create multiple extraneous Domain Admins
+    This uses the Powershell Active Directory module's New-ADUser and Add-ADGroupMember to create multiple extraneous Domain Admins.
+    Supplement users with a GenericAll ACL granted to EVERYONE so multiple other users of choice can be elevated
     Function: Add-PersistDomainAdmins
     Author: Altoid0, Twitter: @Altoid0day
     Version: 1.0
     .DESCRIPTION
-    Just spam the DC with extra Domain Admins for persistence
+    Just spam the DC with extra Domain Admins and configure GenericAll for EVERYONE for persistence
     .NOTES
     Simple trick to add complexity can be the addition of spaces at the end of usernames. 
     They are invisible from the CLI making it difficult to detect and delete users without the presence of a GUI.
